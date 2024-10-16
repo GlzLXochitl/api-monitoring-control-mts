e05b7c75d6253ba54fcee413eac392c2c388b0e4
glzlxochitl <xochitlgl4903@gmail.com>
2024-10-16 11:00:02 -0600

    update only dataBase file

--- dataBase/db_yaskawa_MMC.sql
+++ dataBase/db_yaskawa_MMC.sql
@@ -5,86 +5,50 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema MMC
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema mmc
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema MMC
+-- Schema mmc
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `MMC` DEFAULT CHARACTER SET utf8 ;
--- -----------------------------------------------------
--- Schema new_schema1
--- -----------------------------------------------------
-USE `MMC` ;
+CREATE SCHEMA IF NOT EXISTS `mmc` ;
+USE `mmc` ;
 
 -- -----------------------------------------------------
--- Table `MMC`.`projects`
+-- Table `mmc`.`projects`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `MMC`.`projects` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `mmc`.`projects` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `identification_number` INT(30) NOT NULL,
-  `delivery_date` DATE NULL,
-  `completed` TINYINT NOT NULL DEFAULT 0,
-  `cost_material` INT NOT NULL,
-  `description` VARCHAR(255) NULL,
+  `delivery_date` DATE NULL DEFAULT NULL,
+  `completed` TINYINT(4) NOT NULL DEFAULT 0,
+  `cost_material` INT(11) NOT NULL,
+  `description` VARCHAR(255) NULL DEFAULT NULL,
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
   `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
   UNIQUE INDEX `identification_number_UNIQUE` (`identification_number` ASC) VISIBLE)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 41;
 
 
 -- -----------------------------------------------------
--- Table `MMC`.`user_type`
+-- Table `mmc`.`assembly`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `MMC`.`user_type` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `type` VARCHAR(15) NOT NULL,
-  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `MMC`.`users`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `MMC`.`users` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `user_type_id` INT UNSIGNED NOT NULL,
-  `user_number` VARCHAR(25) NOT NULL,
-  `email` VARCHAR(65) NOT NULL,
-  `password` VARCHAR(30) NOT NULL,
-  `deleted_at` TIMESTAMP NULL,
-  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  `updated_at` TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
-  INDEX `user_type_id_fk_idx` (`user_type_id` ASC) VISIBLE,
-  CONSTRAINT `user_type_user_id_fk`
-    FOREIGN KEY (`user_type_id`)
-    REFERENCES `MMC`.`user_type` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `MMC`.`assembly`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `MMC`.`assembly` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `project_id` INT UNSIGNED NOT NULL,
+CREATE TABLE IF NOT EXISTS `mmc`.`assembly` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `project_id` INT(10) UNSIGNED NOT NULL,
   `identification_number` VARCHAR(50) NOT NULL,
-  `description` VARCHAR(255) NULL,
-  `delivery_date` DATE NULL,
-  `completed_date` DATE NULL,
+  `description` VARCHAR(255) NULL DEFAULT NULL,
+  `delivery_date` DATE NULL DEFAULT NULL,
+  `completed_date` DATE NULL DEFAULT NULL,
   `price` DECIMAL(10,2) NOT NULL,
   `currency` VARCHAR(5) NOT NULL,
-  `completed_assembly` TINYINT NOT NULL DEFAULT 0,
+  `completed_assembly` TINYINT(4) NOT NULL DEFAULT 0,
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
   `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
   PRIMARY KEY (`id`),
@@ -92,56 +56,58 @@ CREATE TABLE IF NOT EXISTS `MMC`.`assembly` (
   INDEX `project_id_fk_idx` (`project_id` ASC) VISIBLE,
   CONSTRAINT `project_id_assembly_fk`
     FOREIGN KEY (`project_id`)
-    REFERENCES `MMC`.`projects` (`id`)
+    REFERENCES `mmc`.`projects` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 9;
 
 
 -- -----------------------------------------------------
--- Table `MMC`.`items`
+-- Table `mmc`.`items`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `MMC`.`items` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `project_id` INT UNSIGNED NULL,
-  `assembly_id` INT UNSIGNED NULL,
+CREATE TABLE IF NOT EXISTS `mmc`.`items` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `project_id` INT(10) UNSIGNED NULL DEFAULT NULL,
+  `assembly_id` INT(10) UNSIGNED NULL DEFAULT NULL,
   `name` VARCHAR(110) NOT NULL,
-  `description` VARCHAR(255) NULL,
-  `quantity` DECIMAL NOT NULL,
+  `description` VARCHAR(255) NULL DEFAULT NULL,
+  `quantity` DECIMAL(10,0) NOT NULL,
   `price` DECIMAL(15,2) NOT NULL,
   `currency` VARCHAR(5) NOT NULL,
-  `arrived_date` DATE NULL,
-  `date_order` DATE NULL,
-  `in_assembly` TINYINT NULL,
-  `number_material` INT NULL,
-  `number_price_item` VARCHAR(25) NULL,
-  `supplier` VARCHAR(55) NULL,
+  `arrived_date` DATE NULL DEFAULT NULL,
+  `date_order` DATE NULL DEFAULT NULL,
+  `in_assembly` TINYINT(4) NULL DEFAULT NULL,
+  `number_material` INT(11) NULL DEFAULT NULL,
+  `number_price_item` VARCHAR(25) NULL DEFAULT NULL,
+  `supplier` VARCHAR(55) NULL DEFAULT NULL,
   `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
   INDEX `project_id_fk_idx` (`project_id` ASC) VISIBLE,
   INDEX `assembly_fk_idx` (`assembly_id` ASC) VISIBLE,
-  CONSTRAINT `project_id_items_fk`
-    FOREIGN KEY (`project_id`)
-    REFERENCES `MMC`.`projects` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
   CONSTRAINT `assembly_items_fk`
     FOREIGN KEY (`assembly_id`)
-    REFERENCES `MMC`.`assembly` (`id`)
+    REFERENCES `mmc`.`assembly` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `project_id_items_fk`
+    FOREIGN KEY (`project_id`)
+    REFERENCES `mmc`.`projects` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 60;
 
 
 -- -----------------------------------------------------
--- Table `MMC`.`stock`
+-- Table `mmc`.`stock`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `MMC`.`stock` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `item_id` INT UNSIGNED NOT NULL,
-  `quantity` DECIMAL NULL,
+CREATE TABLE IF NOT EXISTS `mmc`.`stock` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `item_id` INT(10) UNSIGNED NOT NULL,
+  `quantity` DECIMAL(10,0) NULL DEFAULT NULL,
   `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
   PRIMARY KEY (`id`),
@@ -149,75 +115,118 @@ CREATE TABLE IF NOT EXISTS `MMC`.`stock` (
   INDEX `item_id_stock_fk_idx` (`item_id` ASC) VISIBLE,
   CONSTRAINT `item_id_stock_fk`
     FOREIGN KEY (`item_id`)
-    REFERENCES `MMC`.`items` (`id`)
+    REFERENCES `mmc`.`items` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 13;
 
 
 -- -----------------------------------------------------
--- Table `MMC`.`bom`
+-- Table `mmc`.`bom`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `MMC`.`bom` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `project_id` INT UNSIGNED NOT NULL,
-  `assembly_id` INT UNSIGNED NOT NULL,
-  `item_id` INT UNSIGNED NULL,
-  `stock_id` INT UNSIGNED NULL,
+CREATE TABLE IF NOT EXISTS `mmc`.`bom` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `project_id` INT(10) UNSIGNED NOT NULL,
+  `assembly_id` INT(10) UNSIGNED NOT NULL,
+  `item_id` INT(10) UNSIGNED NULL DEFAULT NULL,
+  `stock_id` INT(10) UNSIGNED NULL DEFAULT NULL,
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
   `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
   PRIMARY KEY (`id`),
-  INDEX `project_id_fk_idx` (`project_id` ASC) VISIBLE,
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  INDEX `project_id_fk_idx` (`project_id` ASC) VISIBLE,
   INDEX `assembly_id_bom_fk_idx` (`assembly_id` ASC) VISIBLE,
   INDEX `stock_id_bom_fk_idx` (`stock_id` ASC) VISIBLE,
+  INDEX `item_id_bom_fk` (`item_id` ASC) VISIBLE,
+  CONSTRAINT `assembly_id_bom_fk`
+    FOREIGN KEY (`assembly_id`)
+    REFERENCES `mmc`.`assembly` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
   CONSTRAINT `item_id_bom_fk`
     FOREIGN KEY (`item_id`)
-    REFERENCES `MMC`.`items` (`id`)
+    REFERENCES `mmc`.`items` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `project_id_bom_fk`
     FOREIGN KEY (`project_id`)
-    REFERENCES `MMC`.`projects` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `assembly_id_bom_fk`
-    FOREIGN KEY (`assembly_id`)
-    REFERENCES `MMC`.`assembly` (`id`)
+    REFERENCES `mmc`.`projects` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `stock_id_bom_fk`
     FOREIGN KEY (`stock_id`)
-    REFERENCES `MMC`.`stock` (`id`)
+    REFERENCES `mmc`.`stock` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 38;
 
 
 -- -----------------------------------------------------
--- Table `MMC`.`users_projects`
+-- Table `mmc`.`user_type`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `MMC`.`users_projects` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `users_id` INT UNSIGNED NOT NULL,
-  `project_id` INT UNSIGNED NOT NULL,
+CREATE TABLE IF NOT EXISTS `mmc`.`user_type` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `type` VARCHAR(15) NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)
+ENGINE = InnoDB
+AUTO_INCREMENT = 4;
+
+
+-- -----------------------------------------------------
+-- Table `mmc`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mmc`.`users` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_type_id` INT(10) UNSIGNED NOT NULL,
+  `user_number` VARCHAR(25) NOT NULL,
+  `email` VARCHAR(65) NOT NULL,
+  `password` VARCHAR(30) NOT NULL,
+  `deleted_at` TIMESTAMP NULL DEFAULT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+  `updated_at` TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP(),
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  INDEX `user_type_id_fk_idx` (`user_type_id` ASC) VISIBLE,
+  CONSTRAINT `user_type_user_id_fk`
+    FOREIGN KEY (`user_type_id`)
+    REFERENCES `mmc`.`user_type` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 16;
+
+
+-- -----------------------------------------------------
+-- Table `mmc`.`users_projects`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mmc`.`users_projects` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `users_id` INT(10) UNSIGNED NOT NULL,
+  `project_id` INT(10) UNSIGNED NOT NULL,
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
   `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
   INDEX `projects_id_fk_idx` (`project_id` ASC) VISIBLE,
   INDEX `fk_users_id_users_projects_idx` (`users_id` ASC) VISIBLE,
-  CONSTRAINT `fk_users_id_users_projects`
-    FOREIGN KEY (`users_id`)
-    REFERENCES `MMC`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
   CONSTRAINT `fk_projects_id_users_projects`
     FOREIGN KEY (`project_id`)
-    REFERENCES `MMC`.`projects` (`id`)
+    REFERENCES `mmc`.`projects` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_users_id_users_projects`
+    FOREIGN KEY (`users_id`)
+    REFERENCES `mmc`.`users` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 15;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
